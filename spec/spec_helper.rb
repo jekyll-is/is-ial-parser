6797bfb2e7b756532bb3c200923056d771223505
Иван Шихалев <shikhalev@gmail.com>
2025-11-02 21:45:19 +0500

    + classifier

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 # spec/spec_helper.rb
 require "bundler/setup"
-require_relative "../lib/ial_parser/tokenizer"
+
 require_relative "../lib/ial_parser/errors"
+require_relative "../lib/ial_parser/tokenizer"
+require_relative "../lib/ial_parser/classifier"
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
